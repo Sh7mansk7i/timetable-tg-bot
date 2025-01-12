cafe2df452d03b3d10b81d53a4a6e85c9d0ce7a4
vladyslave <svlad2331@gmail.com>
2025-01-13 00:05:05 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
-  );
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const [day, setDay] = useState(new Date().getDay());
+
+    const next = () => {
+        setDay((prevDay) => (prevDay + 1) % 7);
+    };
+
+    const prev = () => {
+        setDay((prevDay) => (prevDay - 1 + 7) % 7);
+    };
+
+    return (
+        <div className="app">
+            <div className="day-display">
+                <button className="nav-button" onClick={prev}>⬅️</button>
+                <span className="day-name">{daysOfWeek[day]}</span>
+                <button className="nav-button" onClick={next}>➡️</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
